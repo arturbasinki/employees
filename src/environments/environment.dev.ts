93f569a4a1915dcb29a2ed23515eae7b24f03198
airbass <artur.basinski@gmail.com>
2022-05-30 20:15:09 +0200

    setting firebase

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  production: true,
-  name: 'prod',
+  production: false,
+  name: 'dev',
   firebase: {
     config: {
       apiKey: 'AIzaSyAzk75x0KPtnHnEKBKEepMvT5gI1KvjjXY',
